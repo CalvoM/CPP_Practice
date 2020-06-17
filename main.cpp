216a6886f6b86dcf7611915ed1b388b3270da478
CalvoM <mwadimemakokha@ieee.org>
2020-06-17 22:50:46 +0300

    Linked List implementation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "Quaternion.h"
+#include "LinkedList.h"
 int main(){
     Quaternion<int> q1;
      std::cin >> q1;
@@ -6,4 +7,22 @@ int main(){
      std::cin >> q2;
     q1 -= q2;
     std::cout << q1;
+    NodeList<std::string> list;
+    list.addToFront("Dear");
+    list.addToFront("Sth");
+    list.addToFront("Say");
+    list.addToFront("You");
+    list.addToFront("Did");
+    list.append("Peace");
+    list.insert(0, "Hey");
+    list.insert(6, "Swrry");
+    list.insert(1, "Ahem");
+    list.insert(7, "Listen Up");
+    if (list.is_empty()) {
+      std::cout << "List is empty" << std::endl;
+    } else {
+      std::cout << "Not empty: List has " << list.length() << " members!"
+                << std::endl;
+    }
+    std::cout << list;
 }
