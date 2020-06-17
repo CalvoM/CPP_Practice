3c289a2a2fa05ea5a3badd66a15a65d73b36c1b2
CalvoM <mwadimemakokha@ieee.org>
2020-06-17 23:07:33 +0300

    correct unnecessary move and add delete method

--- includes/LinkedList.h
+++ includes/LinkedList.h
@@ -29,6 +29,7 @@ class NodeList{
         bool is_empty();
         void addToFront(_T el);
         void append(_T el);
+        void del(int index);
         void insert(int index,_T el);
         int length() const;
         ~NodeList(){
@@ -40,7 +41,7 @@ class NodeList{
 template <typename _T>
 std::ostream& operator<<(std::ostream &out, const NodeList<_T>&n_list){
     Node<_T>* tmp;
-    tmp = std::move(n_list.m_head);
+    tmp = n_list.m_head;
     while(tmp!=nullptr){
         out <<"|" <<tmp->data;
         tmp = tmp->next;
